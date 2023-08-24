ab5f35ec71af308424492500e5f937698e0ab466
Hohyun, Kang <hohyun@entwiz.com>
2023-08-24 13:58:11 +0900

    이미지 테스트

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   reactStrictMode: true,
   assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  images: {
+    loader: 'imgix',
+    path: 'https://webhohyun.github.io/nextcalendar/',
+  },
 };
 
 module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import {prefix} from "../../config/config";
+import Image from "next/image";
+
+
 export default function Home() {
   return (
-    <>final test111</>
+    <>
+    final test00
+    <Image src={`${prefix}/next.svg`} alt="Vercel Logo" width={72} height={16} />
+    </>
   )
 }
